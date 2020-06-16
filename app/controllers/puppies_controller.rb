283442e8b76ea39f7cb7cf5ff5bd23a835d2e825
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-16 15:34:27 -0400

    Done.

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,50 @@
 class PuppiesController < ApplicationController
    
+    # display list of all the puppies
+    get '/puppies' do 
+        @puppies = Puppy.all
+        erb :index 
+    end
+
+    # Form for creating a puppy
+    get '/puppies/new' do 
+        erb :new
+    end
+
+    # display single puppy info
+    get '/puppies/:id' do 
+        @puppy = find_puppy(params)
+        erb :show 
+    end 
+
+    # Create a puppy 
+    post '/puppies' do 
+        puppy = Puppy.create(params)
+        redirect "/puppies/#{puppy.id}"
+    end 
+
+    # Form for editing an existing puppy
+    get '/puppies/:id?edit' do 
+        @puppy = find_puppy(params)
+        erb :edit 
+    end 
+
+    # Edit an existing puppy 
+    patch '/puppies/:id' do 
+        puppy = find_puppy(params) 
+        puppy.update(name: params[:name], breed:params[:breed])
+        redirect "/puppies/#{puppy.id}"
+    end 
+
+    # Delete an existing puppy 
+    delete '/puppies/:id' do 
+        puppy = find_puppy(params)
+        puppy.destroy 
+        redirect "/puppies"
+    end 
+
+    def find_puppy(params) 
+        Puppy.find(params[:id])
+    end 
+
 end
